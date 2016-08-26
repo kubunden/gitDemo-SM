26c02ec5455c9449e208774baeacc7b5e50ecf7a
kubunden <stefyipad@gmail.com>
2016-08-26 17:08:38 +0200

    constant and bicolor Changed

---
+++ GitHubDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GitHubDemo
+//
+//  Created by Stefano Mainardi on 26/08/16.
+//  Copyright © 2016 Stefano Mainardi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    let userName = "dummyUser"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
